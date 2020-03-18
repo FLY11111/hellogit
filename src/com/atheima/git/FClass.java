d507650e13ba41c2fcd7480be7eaa4e2be516289
fly24 <963271114@qq.com>
2020-03-18 18:34:38 +0800

    提交本地仓

---
+++ src/com/atheima/git/FClass.java
@@ -0,0 +1,12 @@
+package com.atheima.git;
+
+public class FClass {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
+
+
+    }
+}
